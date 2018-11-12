4a1ace8ffd604d9d8a8bdbbd498d4d1664860257
hechunxiao <ccg7362@163.com>
2018-11-12 15:02:30 +0800

    优化定时器类

--- myself-web/src/main/java/com/sipingsoft/core/timer/SecurityCodeTimer.java
+++ myself-web/src/main/java/com/sipingsoft/core/timer/SecurityCodeTimer.java
@@ -1,6 +1,7 @@
 package com.sipingsoft.core.timer;
 
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 import org.springframework.util.ResourceUtils;
 
 import java.io.File;
@@ -10,9 +11,13 @@ import java.io.FileNotFoundException;
  * @author HeChunXiao
  * @since 2018-11-02 下午 3:26
  */
+@Component
 public class SecurityCodeTimer {
 
 
+    /**
+     * 清除验证码的定时器(清除周期一天:每天凌晨清空保存验证码图片的文件夹)
+     */
     @Scheduled(cron = "0 0 0 1/1 * ?")
     public void deletePic(){
         try {
