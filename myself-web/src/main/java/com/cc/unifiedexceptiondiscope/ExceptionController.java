e82d3212fbda516246623c69910736442ce719d4
cc <ccg7362@163.com>
2019-03-17 03:38:32 +0800

    格式调整

--- myself-web/src/main/java/com/cc/unifiedexceptiondiscope/ExceptionController.java
+++ myself-web/src/main/java/com/cc/unifiedexceptiondiscope/ExceptionController.java
@@ -18,7 +18,6 @@ import java.util.Map;
  *
  * @author HeChunXiao
  * @since 2018-11-09 上午 10:03
- *
  */
 
 @ControllerAdvice
@@ -27,32 +26,33 @@ public class ExceptionController {
 
     /**
      * 统一错误处理
+     *
      * @return 错误提示消息
      */
     @ExceptionHandler(WithaleafException.class)
     @ResponseBody
-    public Map<String, Object> withaleafExceptionDiscope( WithaleafException e) {
-        Map<String,Object> map = new HashMap<>();
-        map.put("code",e.getCode());
-        map.put("errorMsg",e.getErrorMsg());
+    public Map<String, Object> withaleafExceptionDiscope(WithaleafException e) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("code", e.getCode());
+        map.put("errorMsg", e.getErrorMsg());
         return map;
     }
 
     @ExceptionHandler(RandomCodeException.class)
     @ResponseBody
-    public ResponseMessage codeException(RandomCodeException e){
-        return new ResponseMessage(500,e.getMessage());
+    public ResponseMessage codeException(RandomCodeException e) {
+        return new ResponseMessage(500, e.getMessage());
     }
 
     @ExceptionHandler(FileNotFoundException.class)
     @ResponseBody
-    public ResponseMessage fileException(FileNotFoundException e){
-        return new ResponseMessage(500,"系统异常,请刷新页面重试:"+e.getMessage());
+    public ResponseMessage fileException(FileNotFoundException e) {
+        return new ResponseMessage(500, "系统异常,请刷新页面重试:" + e.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
     @ResponseBody
-    public ResponseMessage exception(Exception e){
-        return new ResponseMessage(500,"系统异常:"+e.getMessage());
+    public ResponseMessage exception(Exception e) {
+        return new ResponseMessage(500, "系统异常:" + e.getMessage());
     }
 }
